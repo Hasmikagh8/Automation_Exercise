4ab26b9f6e27c4dda12bc5d5df071e9c835190a4
Hasmikagh8 <testp9808@gmail.com>
2025-02-22 02:59:04 +0400

    Home Page v1

--- cypress/Pages/BasePage.js
+++ cypress/Pages/BasePage.js
@@ -1,17 +1,48 @@
 class BasePage {
+  Names = {
+    title: "AutomationExercise",
+    subtitle: "Full-Fledged practice website for Automation Engineers",
+    text: "All QA engineers can use this website for automation practice and API testing either they are at beginner or advance level. This is for everybody to help them brush up their automation skills.",
+    btnTestCases: "Test Cases",
+    btnAPIList: "APIs list for practice",
+    featuresItems: "Features Items",
+    home: "Home",
+    products: "Products",
+    featuresItemsCount: "34",
+  };
 
   Locators = {
+    shopMenu: ".shop-menu.pull-right",
+    style: "style",
+    selectedFromMenu: "color: orange",
+    active: "active",
     leftControl: ".left control-carousel hidden-xs",
     rigChtontrol: ".right control-carousel hidden-xs",
     carouselIndicators: ".carousel-indicators",
-    carouselInner: ".carousel-inner"
+    carouselInner: ".carousel-inner",
+    carouselRight: ".fa fa-angle-right",
+    carouselLeft: ".fa fa-angle-left",
+    h1: "h1",
+    h2: "h2",
+    testCasesBtn: ".test_cases_list",
+    testCasesLink: "/test_cases",
+    APIsListBtn: ".apis_list",
+    APIsListLink: "/api_list",
+    featuresItemsCl: ".features_items",
+    itemsClass: ".col-sm-4",
+    dataProductID: "data-product-id",
+    selectedProductLink: "/product_details",
+    chooseProduct: ".choose",
+    aViewProduct: "View Product",
+    viewProduct: ".view-product",
+    getProductPicture: "/get_product_picture/",
   };
 
-  images = {image1: 'img[src="/static/images/home/girl1.jpg"]',
-    image2 : '[img[src="/static/images/home/girl2.jpg"]',
-    image3:'[img[src="/static/images/home/girl3.jpg"]',
-  };
-  
+  images = [
+    'img[src="/static/images/home/girl2.jpg"]',
+    'img[src="/static/images/home/girl1.jpg"]',
+    'img[src="/static/images/home/girl3.jpg"]',
+  ];
 }
 
 export const basePage = new BasePage();
--- cypress/e2e/UI/HomePage.cy.js
+++ cypress/e2e/UI/HomePage.cy.js
@@ -1,20 +1,128 @@
 import { basePage } from "../../Pages/BasePage";
 
+let productIdConst = 0;
+
 describe("Automation Exercise carousel part", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
   it("Verify Carousel Functionality", () => {
+    //Verify that Home section is selected
+    cy.get(basePage.Locators.shopMenu)
+      .find('a[href="/"]')
+      .and("contain", basePage.Names.home)
+      .should("have.attr", basePage.Locators.style) // Ensure the style attribute exists
+      .and("include", basePage.Locators.selectedFromMenu);
+
     for (let i = 0; i < 3; i++) {
       cy.get(basePage.Locators.carouselIndicators)
         .find("li")
         .eq(i)
-        .should("have.class", "active");
+        .should("have.class", basePage.Locators.active);
 
+      cy.get(basePage.Locators.h1).should("contain", basePage.Names.title);
+      cy.get(basePage.Locators.h2).should("contain", basePage.Names.subtitle);
+      cy.contains(basePage.Names.text);
+      cy.contains("button", basePage.Names.btnTestCases).should("exist");
+      // .and("be.visible");
+      cy.contains("button", basePage.Names.btnAPIList).should("exist");
+      //.and("be.visible");
       // cy.get(".carousel-inner").find("div").eq(i).should("have.class", "item active");
-     // cy.get(basePage.images.image[i]).should("be.visible");
-      cy.wait(2000);
+      cy.get(basePage.images[i]).should("be.visible");
+      cy.wait(3500);
     }
+  });
+
+  it.skip("Verify Carousel Functionality by >", () => {
+    cy.get(basePage.images[0]).should("be.visible");
+
+    for (let j = 0; j < 3; j++) {
+      cy.get("a.right.control-carousel.hidden-xs").click();
+
+      if (j < 2) {
+        cy.get(basePage.images[j + 1]).should("be.visible");
+      } else {
+        cy.get(basePage.images[0]).should("be.visible");
+      }
+    }
+  });
+
+  it("Verify buttons Test Cases and APIs Lists buttons functionality", () => {
+    cy.contains("button", basePage.Names.btnTestCases).click();
+    cy.url().should("include", basePage.Locators.testCasesLink);
+    cy.go("back");
+
+    cy.get(basePage.Locators.APIsListBtn)
+      .should("have.attr", "href")
+      .and("include", basePage.Locators.APIsListLink);
+    cy.contains("button", basePage.Names.btnAPIList).click();
+    cy.url().should("include", basePage.Locators.APIsListLink);
+    cy.go("back");
+  });
+
+  it("Verify 'Features Items' has 34 items and main data are visible", () => {
+    cy.get(basePage.Locators.featuresItemsCl)
+      .find("img")
+      .should("have.length", basePage.Names.featuresItemsCount);
+
+    for (let i = 0; i < basePage.Names.featuresItemsCount; i++) {
+      cy.get(basePage.Locators.featuresItemsCl)
+        .find("img")
+        .eq(i)
+        .should("be.visible");
+    }
+  });
+
+  it("Verify any item selection", () => {
+    const itemSelection = Math.floor(
+      Math.random() * basePage.Names.featuresItemsCount
+    );
+    cy.log(itemSelection);
+
+    cy.get(basePage.Locators.featuresItemsCl)
+      .find(basePage.Locators.itemsClass)
+      .should("have.length", basePage.Names.featuresItemsCount);
+
+    cy.get(basePage.Locators.featuresItemsCl)
+      .find(basePage.Locators.itemsClass)
+      .eq(itemSelection)
+      .find("a")
+      .invoke("attr", basePage.Locators.dataProductID)
+      .then((productId) => {
+        productIdConst = productId;
+        cy.log(productIdConst);
+      });
+
+    cy.get(basePage.Locators.featuresItemsCl)
+      .find(basePage.Locators.itemsClass)
+      .eq(itemSelection)
+      .find(basePage.Locators.chooseProduct)
+      .find("a")
+      .and("contain", basePage.Locators.aViewProduct)
+      .click();
+
+    //Verify that Product section is selected
+    cy.then(() => {
+      cy.get(".shop-menu.pull-right")
+        .find('a[href="/products"]')
+        .should("contain", basePage.Names.products)
+        .should("have.attr", basePage.Locators.style) // Ensure the style attribute exists
+        .and("include", basePage.Locators.selectedFromMenu);
+    });
+
+    cy.then(() => {
+      cy.url().should(
+        "include",
+        `${basePage.Locators.selectedProductLink}/${productIdConst}`
+      );
+    });
+
+    // check the image in opened page
+    cy.then(() => {
+      cy.get(basePage.Locators.viewProduct)
+        .find("img")
+        .should("have.attr", "src", `${basePage.Locators.getProductPicture}${productIdConst}`);
+    });
   });
 });
